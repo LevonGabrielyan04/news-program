4debc8a87842180647d022838e0a4f5211b677ab
Levon Gabrielyan <levongabrielyan44@gmail.com>
2023-03-25 13:35:24 +0400

    second commit

---
+++ Article programm/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿namespace Article_programm.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        public string Tags { get; set; }
+        public int Views { get; set; }
+    }
+}
---
+++ Article programm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Article_programm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
